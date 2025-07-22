dde9548dac15d094160ef3b95e3d0f54234e7fc0
Rafael Irades <rafaelirades@gmail.com>
2025-07-22 12:55:58 -0300

    feat simple conflict check

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello")
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: consol <file>")
+		return
+	}
+
+	file, err := os.ReadFile(os.Args[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	content := string(file)
+
+	if !strings.Contains(content, "<<<<<<< HEAD") {
+		fmt.Println("No conflicts found!")
+		return
+	}
 }
