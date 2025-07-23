0217518d4046c12e7b743e7fe78fae37d1125672
Rafael Irades <rafaelirades@gmail.com>
2025-07-23 09:39:42 -0300

    feat initial conflict parse file

---
+++ parser/conflict.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"regexp"
+)
+
+type Conflict struct {
+	Startline int
+	Endline   int
+	Ours      string
+	Theirs    string
+}
+
+var (
+	startConflict = regexp.MustCompile(`(?m)^<{7} \S*`)
+	endConflict   = regexp.MustCompile(`(?m)^>{7} \S*`)
+)
+
+func HasConflict(content string) {
+	if !startConflict.MatchString(content) {
+		return false
+	}
+	return true
+}
+
+func ParseFile(content string) ([]Conflict, string) {
+	// TODO
+	return []Conflict{}
+}
