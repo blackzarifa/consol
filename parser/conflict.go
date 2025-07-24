3e6e0093280ef211c53875edaf2f3202865a6933
Rafael Irades <rafaelirades@gmail.com>
2025-07-24 11:46:23 -0300

    improved regex

--- parser/conflict.go
+++ parser/conflict.go
@@ -13,9 +13,9 @@ type Conflict struct {
 }
 
 var (
-	conflictStart     = regexp.MustCompile(`(?m)^<{7} \S*`)
-	conflictSeparator = regexp.MustCompile(`(?m)^={7}$`)
-	conflictEnd       = regexp.MustCompile(`(?m)^>{7} \S*`)
+	conflictStart     = regexp.MustCompile(`(?m)^<{7}( .*)?`)
+	conflictSeparator = regexp.MustCompile(`(?m)^={7}`)
+	conflictEnd       = regexp.MustCompile(`(?m)^>{7}( .*)?`)
 )
 
 func HasConflict(content string) bool {
@@ -29,7 +29,12 @@ func ParseFile(content string) ([]Conflict, string) {
 	var conflicts []Conflict
 
 	startIndexes := conflictStart.FindAllStringIndex(content, -1)
+	separatorIndexes := conflictSeparator.FindAllStringIndex(content, -1)
+	endIndexes := conflictEnd.FindAllStringIndex(content, -1)
+
 	fmt.Println(startIndexes)
+	fmt.Println(separatorIndexes)
+	fmt.Println(endIndexes)
 
 	return conflicts, ""
 }
