6287da363fb2692a4db1d2e5275f164c8a11cea8
blackzarifa <rafaelirades@gmail.com>
2025-08-16 22:53:29 -0300

    format

--- main.go
+++ main.go
@@ -59,13 +59,10 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-
 	case tea.KeyMsg:
 		switch msg.String() {
-
 		case "ctrl+c", "q":
 			return m, tea.Quit
-
 		}
 	}
 
