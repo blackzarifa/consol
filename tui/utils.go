164a471a213e0d3270b33f4ce1e4cca112a534d2
blackzarifa <rafaelirades@gmail.com>
2025-08-31 00:39:21 -0300

    refac conflict resolution logic

--- tui/utils.go
+++ tui/utils.go
@@ -3,6 +3,8 @@ package tui
 import (
 	"slices"
 	"strings"
+
+	"github.com/blackzarifa/consol/parser"
 )
 
 func RemoveIndex[T any](slice []T, index int) []T {
@@ -10,47 +12,55 @@ func RemoveIndex[T any](slice []T, index int) []T {
 	return slice
 }
 
-func (m model) calculateOffset(cursor int) int {
-	centered := cursor - m.contentSize/2
-	maxOffset := len(m.normalized) - m.contentSize - 1
-	return max(0, min(centered, maxOffset))
-}
-
 func (m *model) resolveConflict(resolution string) {
 	if len(m.conflicts) == 0 {
 		return
 	}
 
-	cIndex := m.currentConflict
-	cc := m.conflicts[cIndex]
+	cc := m.conflicts[m.currentConflict]
 	resLines := strings.Split(resolution, "\n")
 
 	m.normalized = slices.Replace(
 		m.normalized, cc.StartLine-1, cc.EndLine, resLines...,
 	)
-	m.conflicts = RemoveIndex(m.conflicts, cIndex)
+	m.conflicts = RemoveIndex(m.conflicts, m.currentConflict)
 
 	originalNumLines := cc.EndLine - cc.StartLine + 1
 	lineDiff := len(resLines) - originalNumLines
 
+	m.updateConflictLines(cc, lineDiff)
+	m.adjustCurrentConflict()
+}
+
+func (m *model) updateConflictLines(
+	resolved parser.Conflict,
+	lineDiff int,
+) {
 	for i := range m.conflicts {
-		if m.conflicts[i].StartLine <= cc.StartLine {
+		if m.conflicts[i].StartLine <= resolved.StartLine {
 			continue
 		}
 		m.conflicts[i].StartLine += lineDiff
 		m.conflicts[i].EndLine += lineDiff
 	}
+}
 
+func (m *model) adjustCurrentConflict() {
 	if len(m.conflicts) == 0 {
 		m.currentConflict = 0
 		return
 	}
 
-	if cIndex >= len(m.conflicts) {
-		cIndex = len(m.conflicts) - 1
+	if m.currentConflict >= len(m.conflicts) {
+		m.currentConflict = len(m.conflicts) - 1
 	}
 
-	m.currentConflict = cIndex
-	m.cursor = m.conflicts[cIndex].StartLine - 1
+	m.cursor = m.conflicts[m.currentConflict].StartLine - 1
 	m.offset = m.calculateOffset(m.cursor)
+}
+
+func (m model) calculateOffset(cursor int) int {
+	centered := cursor - m.contentSize/2
+	maxOffset := len(m.normalized) - m.contentSize - 1
+	return max(0, min(centered, maxOffset))
 }
